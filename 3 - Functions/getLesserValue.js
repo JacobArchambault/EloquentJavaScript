34da0dafa8826ac44b44df09c57c58464b2e5076
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-19 21:22:20 -0400

    Separate out chapter 3 functions into their own files

---
+++ 3 - Functions/getLesserValue.js
@@ -0,0 +1,9 @@
+getLesserValue(4, 2);
+
+function getLesserValue(first, second) {
+	if (first < second) {
+		return first
+	} else {
+		return second
+	}
+}
--- 3 - Functions/getParityRecursive.js
+++ 3 - Functions/getParityRecursive.js
@@ -1,10 +1,4 @@
-function getLesserValue(first, second) {
-	if (first < second) {
-		return first
-	} else {
-		return second
-	}
-}
+getParityRecursive(32);
 
 function getParityRecursive(number) {
 	let parity;
