55037f7d2fd79f28efb9e324b8bce976254f8ee3
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-22 12:54:02 -0400

    Separate out functions in reverseArray.js to be more fine-grained.

--- 4 - Data Structures. Objects and Arrays/reverseArray.js
+++ 4 - Data Structures. Objects and Arrays/reverseArray.js
@@ -1,7 +1,7 @@
 function reverseArray(array){
 	let newArray = [];
-	for (let i = 0; i < array.length; i++) {
-		newArray.unshift(array[i]);
+	for (let arrayNumber of array) {
+		newArray.unshift(arrayNumber);
 	}
 	return newArray;
 };
@@ -10,18 +10,23 @@ let oldArray = [1, 2, 3, 4, 5];
 reverseArray(oldArray);
 
 function reverseArrayInPlace(array){
-	let newArray = [];
-	for (let arrayNumber of array)  {
-		newArray.unshift(arrayNumber);
-	}
-	while (array.length > 0) {
-		array.pop();
-	}
-	for (let number of newArray) {
-		array.push(number);
-	}
+	let tempArray = reverseArray(array);
+	emptyArray(array);
+	pushAll(tempArray, array);
 };
 
 let myArray = [2,4,6,8,10];
 reverseArrayInPlace(myArray);
 console.log(myArray);
+
+function emptyArray(array) {
+	while (array.length > 0) {
+		array.pop();
+	}	
+}
+
+function pushAll(fromArray, toArray) {
+	for (let number of fromArray) {
+	toArray.push(number);
+	}
+}
