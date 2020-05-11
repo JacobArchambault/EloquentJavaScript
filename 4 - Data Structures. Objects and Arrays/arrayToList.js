1fc451247a390a6a58c2e386f3982b538e387e83
Jacob Archambault <jacob.archambault@waystar.com>
2020-05-11 14:02:13 -0400

    Convert arrayToList while loop to for loop

--- 4 - Data Structures. Objects and Arrays/arrayToList.js
+++ 4 - Data Structures. Objects and Arrays/arrayToList.js
@@ -1,18 +1,12 @@
 function arrayToList(array) {
-	// declare list
-	let list = {value: null, rest: null};
+	// initialize node and declare list
 	let node = null;
-	// declare and initialize decrementer
-	let d = array.length - 1;
-	
-	// Prepend nodes to list until reaching the beginning of array.
-	while (d >= 0) {
+	let list;
+	for (d = array.length - 1; d >= 0; d--) {
 		// set value of list to element at array index.
 		list = {value: array[d], rest: node};
 		// set node value to entire list.
-		node = list;
-		// decrement.
-		d--;
+		node = list;		
 	}
 	return list;
 }
