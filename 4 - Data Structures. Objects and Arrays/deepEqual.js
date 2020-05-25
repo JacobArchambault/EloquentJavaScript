b950f0024c99faf3a9b39a03efe5a5bca09b4eb7
Jacob Archambault <jacob.archambault@waystar.com>
2020-05-25 14:59:23 -0400

    Add recursion to deepEqual function comparing object properties

--- 4 - Data Structures. Objects and Arrays/deepEqual.js
+++ 4 - Data Structures. Objects and Arrays/deepEqual.js
@@ -3,21 +3,26 @@ function deepEqual(a, b){
     	return a === b;
     }
 	else {
+		let areEqual = true;
+		
 	    // Put object properties in an array
     	let aProperties = Object.keys(a); 
     	let bProperties = Object.keys(b);
 
-    	// check Length to ensure objects have the same number of properties
-    	if (!areSameLength(aProperties, bProperties)) return false;
-
-        for (let key of aProperties) {
-        	if (a[key] !== b[key]) return false;
-        }
-        return true;
+    	// check Length to ensure objects have the same number of properties. Helps rule out cases where a's properties are only a subset of b's.
+    	if (!areSameLength(aProperties, bProperties)) {
+    		areEqual = false;
+    	} else {
+    		// Check that objects have same property values.
+			for (let key of aProperties) {
+				if (!deepEqual(a[key], b[key])) return false;
+			}
+    	}
+		return areEqual;
 	}
 }
-let obj1 = {a: "foo", b: true, c: 123};
-let obj2 = {b: true, c: 123, a: "foo"};
+let obj1 = {a: {propName: "foo"}, b: true, c: 123};
+let obj2 = {b: true, c: 123, a: {propName: "foo"}};
 deepEqual(obj1, obj2);
 	
 function areSameLength(obj, ...args) {
