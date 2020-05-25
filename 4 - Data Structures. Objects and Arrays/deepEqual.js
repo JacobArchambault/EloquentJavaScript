8abbdb82ca1f08a9e16e0d39067fee1f1b5db1d9
Jacob Archambault <jacob.archambault@waystar.com>
2020-05-25 18:23:10 -0400

    To increase readability, replace single return version of deepEqual function with a multiple-return version using guard clauses

--- 4 - Data Structures. Objects and Arrays/deepEqual.js
+++ 4 - Data Structures. Objects and Arrays/deepEqual.js
@@ -1,29 +1,25 @@
-function deepEqual(a, b){
-    if (a === null || b === null || typeof(a) !== 'object' || typeof(b) !=='object') {
-    	return a === b;
-    }
-	else {
-		let areEqual = true;
-		
-	    // Put object properties in an array
-    	let aProperties = Object.keys(a); 
-    	let bProperties = Object.keys(b);
-
-    	// check Length to ensure objects have the same number of properties. Helps rule out cases where a's properties are only a subset of b's.
-    	if (!areSameLength(aProperties, bProperties)) {
-    		areEqual = false;
-    	} else {
-    		// Check that objects have same property values.
-			for (let key of aProperties) {
-				if (!deepEqual(a[key], b[key])) return false;
-			}
-    	}
-		return areEqual;
-	}
-}
 let obj1 = {a: {propName: "foo"}, b: true, c: 123};
 let obj2 = {b: true, c: 123, a: {propName: "foo"}};
 deepEqual(obj1, obj2);
+
+function deepEqual(a, b){
+	// Directly compare nulls and non-objects for equality.
+    if (a === null || b === null || typeof(a) !== 'object' || typeof(b) !=='object') {return a === b;}
+	
+	// To allow easy iteration over them in order to check their quantity, then recursively check their values for equality, put object property keys into arrays.
+	let aProperties = Object.keys(a); 
+	let bProperties = Object.keys(b);
+
+	// check length to ensure objects have the same number of properties. Helps rule out cases where a's property keys with their values constitute a subset of b's.
+	if (!areSameLength(aProperties, bProperties)) {return false;} 
+
+	// Recursively check that objects have same property values.
+	let areEqual = true;
+	for (let key of aProperties) {
+		if (!deepEqual(a[key], b[key])) return false;
+	}
+	return areEqual;
+}
 	
 function areSameLength(obj, ...args) {
 	let sameLength = true;
