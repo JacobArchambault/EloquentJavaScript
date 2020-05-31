3ffb7bceda726ad03ea09422618a5e449a2931f0
Jacob Archambault <jacob.archambault@waystar.com>
2020-05-31 15:45:21 -0400

    Add alternative version of 'every' function using array.prototype.some()

--- 5 - Higher-Order Functions/every.js
+++ 5 - Higher-Order Functions/every.js
@@ -9,4 +9,11 @@ function every(array, predicate) {
 	return isNonEmpty && result;
 }
 
-every([1, 2, "3"], Number.isInteger);
+function every2(array, predicate) {
+    let isNonEmpty = (array.length > 0);
+    let isTrueForEach = !array.some(element => !predicate(element));
+	return isNonEmpty && isTrueForEach;
+}
+
+every([1, 2, 3], Number.isInteger);
+every2([1, 2, "3"], Number.isInteger);
